a11850134b0311f20e15af3d8f6b1230467dc7a6
BreBusby <brebusby13@gmail.com>
2018-05-10 23:28:55 -0500

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Good Burger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Old Hamburger');
